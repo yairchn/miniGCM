dd032feb026344a0b0262d8591c12731d1592fe8
yairchn <yairchn@caltech.edu>
2020-12-10 15:08:15 -0800

    make the initial state of HS moist for multiplayers

--- Cases.py
+++ Cases.py
@@ -131,9 +131,6 @@ class HeldSuarez_moist(CasesBase):
 
     def initialize(self, Pr, Gr, PV, namelist):
         self.Base_pressure = 100000.0
-        PV.P_init        = [Pr.p1, Pr.p2, Pr.p3, Pr.p_ref]
-        PV.T_init        = [245.0, 282.0, 303.0]
-
         PV.QT_0 = namelist['forcing']['initial_surface_qt']
         Pr.T_0 = namelist['thermodynamics']['triple_point_temp']
         Pr.P_hw = namelist['thermodynamics']['verical_half_width_of_the_q']
@@ -149,11 +146,19 @@ class HeldSuarez_moist(CasesBase):
         Pr.Dtheta_z = namelist['forcing']['lapse_rate']
         Pr.Tbar0 = namelist['forcing']['relaxation_temperature']
         Gamma = namelist['forcing']['Gamma_init']
+        z = np.linspace(0,20000,200)
+
+        PV.P_init        = [Pr.p1, Pr.p2, Pr.p3, Pr.p_ref]
+        PV.T_init        = [245.0, 282.0, 303.0]
+        Tv_ = np.zeros((len(Gr.lat[:,0]), len(z), Pr.n_layers),  dtype=np.double, order='c')
+        p = np.zeros((len(Gr.lat[:,0]), len(z)),  dtype=np.double, order='c')
+        PV.Vorticity.values  = np.zeros((Pr.nlats, Pr.nlons, Pr.n_layers),  dtype=np.double, order='c')
+        PV.Divergence.values = np.zeros((Pr.nlats, Pr.nlons, Pr.n_layers),  dtype=np.double, order='c')
+        PV.P.values          = np.multiply(np.ones((Pr.nlats, Pr.nlons, Pr.n_layers+1), dtype=np.double, order='c'),PV.P_init)
 
         T_0 = 0.5 * (Pr.T_equator + Pr.T_pole)
         B = (T_0 - Pr.T_pole) / T_0 / Pr.T_pole
         C = 0.5 * (Pr.init_k + 2.0)*(Pr.T_equator-Pr.T_pole)/Pr.T_equator/Pr.T_pole
-        z = np.linspace(0,20000,200)
 
         tau_z_1 = np.exp(Gamma * z / T_0)
         tau_z_2 = 1.0 - np.multiply(2.0, np.power(np.divide(z / 2.0, Pr.Rd * T_0 / Pr.g),2.0))
@@ -163,62 +168,31 @@ class HeldSuarez_moist(CasesBase):
         tau_int_1 = np.multiply(1.0 / Gamma , (tau_z_1 - 1.0)) + np.multiply(np.multiply(B , z ), tau_z_3)
         tau_int_2 = np.multiply(np.multiply(C , z) ,tau_z_3);
         I_T = np.power(np.cos(Gr.lat[:,0]),Pr.init_k) - Pr.init_k/(Pr.init_k + 2.0) * np.power(np.cos(Gr.lat[:,0]),Pr.init_k+2.0)
-        T = np.zeros((len(Gr.lat[:,0]), len(z)),  dtype=np.double, order='c')
-        Tv = np.zeros((len(Gr.lat[:,0]), len(z)),  dtype=np.double, order='c')
-        p = np.zeros((len(Gr.lat[:,0]), len(z)),  dtype=np.double, order='c')
-        q_tot = np.zeros((len(Gr.lat[:,0]), len(z)),  dtype=np.double, order='c')
-        qv_star = np.zeros((len(Gr.lat[:,0]), len(z)),  dtype=np.double, order='c')
-        T1_ = np.add(np.zeros((len(Gr.lat[:,0]), len(z)),  dtype=np.double, order='c'), np.nan)
-        QT1_ = np.add(np.zeros((len(Gr.lat[:,0]), len(z)),  dtype=np.double, order='c'), np.nan)
-        T2_ = np.add(np.zeros((len(Gr.lat[:,0]), len(z)),  dtype=np.double, order='c'), np.nan)
-        QT2_ = np.add(np.zeros((len(Gr.lat[:,0]), len(z)),  dtype=np.double, order='c'), np.nan)
-        T3_ = np.add(np.zeros((len(Gr.lat[:,0]), len(z)),  dtype=np.double, order='c'), np.nan)
-        QT3_ = np.add(np.zeros((len(Gr.lat[:,0]), len(z)),  dtype=np.double, order='c'), np.nan)
 
         for i in range(len(Gr.lat[:,0])):
             for j in range(len(z)):
-                Tv[i,j] = np.power(tau_1[j] - tau_2[j] * I_T[i],-1.0)
                 p[i,j] = Pr.p_ref * np.exp(-Pr.g / Pr.Rd * (tau_int_1[j] - tau_int_2[j] * I_T[i]))
-        for i in range(len(Gr.lat[:,0])):
-            for j in range(len(z)):
-                q_tot[i,j] = PV.QT_0 * np.exp(-(Gr.lat[i,0] / Pr.phi_hw)**4.0) * np.exp(-((p[i,j]/Pr.p_ref - 1.0) * Pr.p_ref / Pr.P_hw)**2.0)
-                T[i,j] = Tv[i,j]/(1+0.608*q_tot[i,j])
-                qv_star[i,j] = (610.78 * 0.622 / p[i,j] * np.exp(-(Pr.Lv/Pr.Rv)*(1.0/T[i,j] - 1.0/T_0)))
-                if p[i,j]>25000.0 and p[i,j]<50000.0:
-                    T1_[i,j] = T[i,j]
-                    QT1_[i,j] = q_tot[i,j]
-                elif p[i,j]>50000.0 and p[i,j]<85000.0:
-                    T2_[i,j] = T[i,j]
-                    QT2_[i,j] = q_tot[i,j]
-                elif p[i,j]>85000.0:
-                    T3_[i,j] = T[i,j]
-                    QT3_[i,j] = q_tot[i,j]
+                for k in range(Pr.n_layers):
+                    if (p[i,j]>PV.P_init[k] and p[i,j]<PV.P_init[k+1]):
+                        Tv_[i,j,k] = np.power(tau_1[j] - tau_2[j] * I_T[i],-1.0)
+                    else:
+                        Tv_[i,j,k] = np.nan
 
-        ql = np.clip(q_tot - qv_star,0.0,None)
-        T1 = np.nanmean(T1_, axis=1)
-        T2 = np.nanmean(T2_, axis=1)
-        T3 = np.nanmean(T3_, axis=1)
-        QT1 = np.nanmean(QT1_, axis=1)
-        QT2 = np.nanmean(QT2_, axis=1)
-        QT3 = np.nanmean(QT3_, axis=1)
-
-        QT_in = np.stack((QT1,QT2,QT3))
-        T_in = np.stack((T1,T2,T3))
-
-        PV.Vorticity.values  = np.zeros((Pr.nlats, Pr.nlons, Pr.n_layers),  dtype=np.double, order='c')
-        PV.Divergence.values = np.zeros((Pr.nlats, Pr.nlons, Pr.n_layers),  dtype=np.double, order='c')
-        PV.P.values          = np.multiply(np.ones((Pr.nlats, Pr.nlons, Pr.n_layers+1), dtype=np.double, order='c'),PV.P_init)
-        Tv                   = np.multiply(np.ones((Pr.nlats, Pr.nlons, Pr.n_layers),   dtype=np.double, order='c'),PV.T_init)
         for k in range(Pr.n_layers):
-            sigma_1 = 0.5*(PV.P_init[k]+PV.P_init[k+1])/PV.P_init[Pr.n_layers]-1.0
-            QT_meridional = np.multiply(PV.QT_0,
-                np.exp(-(Gr.lat[:,0]/Pr.phi_hw)**4.0)*np.exp(-(sigma_1*(Pr.p_ref/Pr.P_hw))**2.0))
-            PV.QT.values[:,:,k] = np.repeat(QT_meridional[:, np.newaxis], Pr.nlons, axis=1)
-            PV.T.values[:,:,k]  = np.divide(Tv[:,:,k],np.add(1.0,0.608*PV.QT.values[:,:,k]))
-            QT_meridional = QT_in[k,:]
-            T_meridional = T_in[k,:]
+            QT_meridional = (PV.QT_0 * np.exp(-(Gr.lat[:,0] / Pr.phi_hw)**4.0)
+                * np.exp(-((PV.P_init[k]/Pr.p_ref - 1.0) * Pr.p_ref / Pr.P_hw)**2.0))
+            T_meridional = np.nanmean(Tv_[:,:,k], axis=1)/(1+0.608*QT_meridional)
             PV.QT.values[:,:,k] = np.repeat(QT_meridional[:, np.newaxis], Pr.nlons, axis=1)
             PV.T.values[:,:,k]  = np.repeat(T_meridional[:, np.newaxis], Pr.nlons, axis=1)
+        plt.figure('T')
+        plt.plot(Gr.lat[:,0],PV.T.values[:,0,0], 'r')
+        plt.plot(Gr.lat[:,0],PV.T.values[:,0,1], 'b')
+        plt.plot(Gr.lat[:,0],PV.T.values[:,0,2], 'g')
+        plt.figure('QT')
+        plt.plot(Gr.lat[:,0],PV.QT.values[:,0,0], 'r')
+        plt.plot(Gr.lat[:,0],PV.QT.values[:,0,1], 'b')
+        plt.plot(Gr.lat[:,0],PV.QT.values[:,0,2], 'g')
+        plt.show()
         # # initilize spectral values
         for k in range(Pr.n_layers):
             PV.P.spectral[:,k]           = Gr.SphericalGrid.grdtospec(PV.P.values[:,:,k])
