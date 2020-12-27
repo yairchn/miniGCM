f0070d7ff29ea15c89efe3176f0d4a43696ba985
yairchn <yairchn@caltech.edu>
2020-12-27 11:11:46 -0800

    WIP

--- Grid.pyx
+++ Grid.pyx
@@ -9,15 +9,14 @@ import time
 import sys
 
 cdef class Grid:
-    def __init__(self, namelist):
+    def __init__(self, Pr, namelist):
 		# setup up spherical harmonic instance, set lats/lons of grid
-        self.SphericalGrid = sph.Spharmt(self.nlons, self.nlats, self.truncation_number, self.rsphere, gridtype='gaussian')
+        self.SphericalGrid = sph.Spharmt(Pr.nlons, Pr.nlats, Pr.truncation_number, Pr.rsphere, gridtype='gaussian')
         self.lon, self.lat = np.meshgrid(self.SphericalGrid.lons, self.SphericalGrid.lats) # these are in radians
         self.longitude = np.degrees(self.lon)
         self.latitude  = np.degrees(self.lat)
         self.longitude_list = self.longitude[1,:]
         self.latitude_list = self.latitude[:,1]
-        self.Coriolis = 2.0*self.Omega*np.sin(self.lat)
         self.Coriolis = 2.0*Pr.Omega*np.sin(self.lat)
         return
 
