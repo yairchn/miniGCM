321cf6afe4bb2686177aa38a1862ba39fd336c7b
yairchn <yairchn@caltech.edu>
2021-01-15 09:25:20 -0800

    add plotting file

---
+++ viz/contour_zonal_mean_T.py
@@ -0,0 +1,43 @@
+import numpy as np
+import netCDF4 as nc
+import pylab as plt
+import argparse
+import os
+
+# command line:
+# python viz/contour_zonal_mean.py zonal_mean_P
+def main():
+    parser = argparse.ArgumentParser(prog='miniGCM')
+    parser.add_argument("varname")
+    args = parser.parse_args()
+    varname = args.varname
+    varname = 'zonal_mean_T'
+
+    folder = os.getcwd() + '/Output.HeldSuarez.310k2/stats/'
+    ncfile = folder + 'Stats.HeldSuarez.Restart_2.nc'
+    data = nc.Dataset(ncfile, 'r')
+
+    lat = np.array(data.groups['coordinates'].variables['latitude'])
+    n = int(np.multiply(data.groups['coordinates'].variables['layers'],1.0))
+
+    lat_list = np.array(data.groups['coordinates'].variables['latitude_list'])
+    var = np.array(data.groups['zonal_mean'].variables[varname])
+    t = np.divide(data.groups['zonal_mean'].variables['t'],3600.0*24.0)
+
+    X, Y = np.meshgrid(t,lat_list)
+    fig = plt.figure(varname)
+    for i in range(n):
+        ax1 = fig.add_subplot(n, 1, i+1)
+        im1 = ax1.contourf(X,Y,np.fliplr(np.rot90(np.squeeze(var[:,:,i]), k=3)),cmap='coolwarm')
+        ax1.set_ylabel('latitude / $\circ$')
+        if i==0: plt.title('Hovmoeller Diagram of $\overline{T}$ / K')
+        if i<n-1:
+            xlabels = [item.get_text() for item in ax1.get_xticklabels()]
+            xempty_string_labels = [''] * len(xlabels)
+            ax1.set_xticklabels(xempty_string_labels)
+        else:
+            ax1.set_xlabel('time / days')
+        fig.colorbar(im1)
+    plt.savefig('zonal_mean.png')
+if __name__ == '__main__':
+    main()
---
+++ viz/timeseries_ps.py
@@ -0,0 +1,54 @@
+import numpy as np
+import netCDF4 as nc
+import pylab as plt
+import argparse
+import os
+
+from math import *
+
+weights=np.cos(2.*pi*np.linspace(-90.,90.,256)/360.)
+weights/=np.sum(weights)
+print('weights.shape and sum', weights.shape, np.sum(weights))
+
+
+# command line:
+# python viz/contour_zonal_mean.py zonal_mean_P
+def main():
+    #parser = argparse.ArgumentParser(prog='miniGCM')
+    #parser.add_argument("varname")
+    #args = parser.parse_args()
+    #varname = args.varname
+    varname = 'zonal_mean_P'
+    runname='410k2'
+    runname='4e5k0'
+    runname='310k2'
+
+    folder = '/home/scoty/miniGCM/Output.HeldSuarez.'+runname+'/stats/'
+    ncfile = folder + 'Stats.HeldSuarez.Restart_2.nc'
+    print('ncfile: ',ncfile)
+    data = nc.Dataset(ncfile, 'r')
+
+    lat = np.array(data.groups['coordinates'].variables['latitude'])
+    n = int(np.multiply(data.groups['coordinates'].variables['layers'],1.0))
+
+    lat_list = np.array(data.groups['coordinates'].variables['latitude_list'])
+    var = np.array(data.groups['zonal_mean'].variables[varname])
+    t = np.divide(data.groups['zonal_mean'].variables['t'],3600.0*24.0)
+
+    ps=var[:,:,2]/100.
+    ps_time=np.zeros(1000)
+    for it in range(0,1000): ps_time[it]=np.sum(ps[it,:]*weights)
+
+    eps=.1
+
+    plt.figure(figsize=(5,2))
+    plt.plot(t,ps_time,'-k')
+    plt.xlabel('time / days')
+    plt.ylabel('$p_s$ / hPa')
+    plt.ylim(1000.-eps,1000.+eps)
+    plt.xlim(0,1000.)
+    plt.title('Area Mean Surface Pressure')
+    plt.tight_layout()
+    plt.savefig('timeseries_ps_'+runname+'.png')
+if __name__ == '__main__':
+    main()
