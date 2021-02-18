49bec648dc69a6172a8b176f16232353f868e4e1
yairchn <yairchn@caltech.edu>
2021-02-18 09:38:35 -0800

    WIP

--- Simulation.pyx
+++ Simulation.pyx
@@ -67,8 +67,8 @@ class Simulation:
             # print('TS.update', time.time() - time0)
             # time0 = time.time()
 
-            wallclocktime = time.time() - start_time
             if self.TS.t%self.Stats.stats_frequency < self.TS.dt:
+                wallclocktime = time.time() - start_time
                 self.LF.update(self.Pr, self.TS, self.DV, self.PV, wallclocktime)
                 self.stats_io()
             if self.TS.t%self.Stats.output_frequency < self.TS.dt:
