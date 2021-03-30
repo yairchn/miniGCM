ba6833e149b36f89f4e27bbbceeefb3271d8db5d
yairchn <yairchn@caltech.edu>
2021-03-30 15:42:19 -0700

    WIP

--- viz/contour_zonal_mean.py
+++ viz/contour_zonal_mean.py
@@ -12,8 +12,8 @@ def main():
     args = parser.parse_args()
     varname = args.varname
 
-    folder = os.getcwd() + '/Output.HeldSuarez.dlayer_moist/stats/'
-    ncfile = folder + 'Stats.HeldSuarez.Restart_11.nc'
+    folder = os.getcwd() + '/Output.HeldSuarez.test_T750hpa/'
+    ncfile = folder + 'Stats.HeldSuarez.nc'
     data = nc.Dataset(ncfile, 'r')
 
     lat = np.array(data.groups['coordinates'].variables['latitude'])
