8fe74dc6424527662d6a3632ff38224b7a15fb9b
yairchn <yairchn@caltech.edu>
2021-06-03 10:47:54 -0700

    WIP

--- viz/plot_zonal_mean.py
+++ viz/plot_zonal_mean.py
@@ -12,7 +12,7 @@ def main():
     args = parser.parse_args()
     varname = args.varname
 
-    folder = os.getcwd() + '/Output.HeldSuarezMoist.cc-__no_rain/stats/'
+    folder = os.getcwd() + '/Output.HeldSuarezMoist.mss005_ac200/stats/'
     ncfile = folder + 'Stats.HeldSuarezMoist.nc'
     data = nc.Dataset(ncfile, 'r')
 
@@ -22,12 +22,12 @@ def main():
     lat_list = np.array(data.groups['coordinates'].variables['latitude_list'])
     var = np.array(data.groups['zonal_mean'].variables[varname])
     t = np.divide(data.groups['zonal_mean'].variables['t'],3600.0*24.0)
-
+    print(np.max(t))
     X, Y = np.meshgrid(t,lat_list)
     fig = plt.figure(varname)
     for i in range(n):
         ax1 = fig.add_subplot(n, 1, i+1)
-        im1 = ax1.plot(np.mean(var[:,:,i], axis = 0), lat_list)
+        im1 = ax1.plot(np.mean(var[400:-1,:,i], axis = 0), lat_list)
         ax1.set_ylabel('degree latitude')
         if i==n-1:
             ax1.set_xlabel(varname)
