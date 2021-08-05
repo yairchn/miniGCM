df92f004ac84edf77e8844fbf29850d5d4e030ce
yairchn <yairchn@caltech.edu>
2021-08-05 18:46:09 +0300

    remove truncation

--- viz/contour_zonal_mean.py
+++ viz/contour_zonal_mean.py
@@ -12,8 +12,8 @@ def main():
     args = parser.parse_args()
     varname = args.varname
 
-    folder = os.getcwd() + '/Output.HeldSuarezMoist.24_mp_update/stats/'
-    ncfile = folder + 'Stats.HeldSuarezMoist.nc'
+    folder = os.getcwd() + '/Output.HeldSuarez.o_truncation/stats/'
+    ncfile = folder + 'Stats.HeldSuarez.nc'
     data = nc.Dataset(ncfile, 'r')
 
     lat = np.array(data.groups['coordinates'].variables['latitude'])
