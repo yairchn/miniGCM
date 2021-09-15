4c03f8a638675284869b7a985eecdd81631304a6
yairchn <yairchn@caltech.edu>
2021-09-15 12:18:57 -0500

    take viz files form master

--- viz/KE_dp_flux_spectra.py
+++ viz/KE_dp_flux_spectra.py
@@ -5,35 +5,50 @@ from math import *
 from matplotlib.transforms import Transform
 from matplotlib.ticker import (AutoLocator, AutoMinorLocator)
 
-Hbar=100.
+path='./res4/'
+#path='./res1/'
+path='./'
 
-ks=np.load('ks.npy')
+ks=np.load(path+'ks.npy')
 Ek=np.copy(ks)*0.
 
 #Layer=0
 #Layer=1
 #Layer=2
 
-for Layer in np.arange(0,3):
+dP=750. # [hPa]
+
 #for Layer in np.arange(0,1):
-    for it in np.arange(280,630,14): Ek+=np.load('Enstrophy_flux_'+str(Layer)+'_0000000'+str(it)+'.npy')/40.
+    #icount=0
+    #for it in np.arange(280,630,14): Ek+=np.load(path+'Ek_flux_dp_'+str(Layer)+'_0000000'+str(it)+'.npy'); icount+=1.
+
+icount=0
+for it in np.arange(280,630,14):
+    Ek+=np.load(path+'Ek_flux_dp_0_0000000'+str(it)+'.npy')
+    Ek+=np.load(path+'Ek_flux_dp_1_0000000'+str(it)+'.npy')
+    Ek+=np.load(path+'Ek_flux_dp_2_0000000'+str(it)+'.npy');
+    icount+=1.
+
+Ek/=icount
+Ek/=dP
 
 
 fig, ax = plt.subplots(constrained_layout=True,figsize=(5,4.))
 
-ax.semilogx(ks[1:700],savgol_filter(Ek[1:700],5,1),'-k',linewidth=2,alpha=0.8,label='Enstrophy flux')
+ax.plot(ks,Ek,'-k',linewidth=2,alpha=0.8)#,label='KE flux')
+#ax.semilogx(ks,Ek,'-k',linewidth=2,alpha=0.8)#,label='KE flux')
 
 ax.set_xlabel('Wavenumber $k$',size='12', fontname = 'Dejavu Sans')
 
-ax.set_ylabel('Eddy Enstrophy Flux [W/kg m$^2$]',size='12', fontname = 'Dejavu Sans')
+ax.set_ylabel('Energy Flux [W/kg]',size='12', fontname = 'Dejavu Sans')
 
 plt.grid(alpha=0.7,color='k',linestyle='dotted',dashes=[1,5 ],linewidth=1,zorder=100)
 
-plt.xlim(1,700)
+#plt.xlim(1,700)
 
-#plt.ylim(1.e-3,10.)
+#plt.ylim(-2.e-7,2.e-7)
 
-plt.legend()
+#plt.legend(loc='lower right')
 
 circumference=6371.*pi*2. # [km]
 
@@ -48,7 +63,7 @@ secax = ax.secondary_xaxis('top', functions=(forward, inverse))
 secax.set_xlabel('Wavelength [km]',size='12', fontname = 'Dejavu Sans')
 plt.tight_layout()
 #plt.show()
-plt.savefig('spectra_enstrophy_flux.pdf',dpi=150)
+plt.savefig('Kinetic_Energy_flux_dp.pdf',dpi=150)
 
 l=forward(ks)
 f=1.e-4 # 1/s
