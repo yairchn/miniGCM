e35c33074cb0d053b0d4786e562aeca9fba0f24c
Josef Schröttle <joe@bingo-ev.de>
2021-10-08 19:51:52 +0200

    added stochastic tropical forcing case to factory

--- Cases.pyx
+++ Cases.pyx
@@ -23,6 +23,8 @@ def CasesFactory(namelist):
         return HeldSuarez(namelist)
     elif namelist['meta']['casename'] == 'HeldSuarezMoist':
         return HeldSuarezMoist(namelist)
+    elif namelist['meta']['casename'] == 'TropicalPlanet':
+        return TropicalPlanet(namelist)
     else:
         print('case not recognized')
     return
@@ -58,6 +60,104 @@ cdef class CaseBase:
         return
 
     cpdef update(self, Parameters Pr, Grid Gr, PrognosticVariables PV, DiagnosticVariables DV, TimeStepping TS):
+        return
+
+cdef class TropicalPlanet(CaseBase):
+    def __init__(self, namelist):
+        # Pr.casename = namelist['meta']['casename']
+        self.Fo  = Forcing.ForcingFactory(namelist)
+        self.Sur = Surface.SurfaceFactory(namelist)
+        self.MP  = Microphysics.MicrophysicsFactory(namelist)
+        self.Co = Convection.ConvectionFactory(namelist)
+        self.Tr = Turbulence.TurbulenceFactory(namelist)
+        return
+
+    cpdef initialize(self, Restart RS, Parameters Pr, Grid Gr, PrognosticVariables PV, TimeStepping TS, namelist):
+        cdef:
+            double [:,:] noise
+
+        Pr.sigma_b = namelist['forcing']['sigma_b']
+        Pr.k_a = namelist['forcing']['k_a']
+        Pr.k_b = namelist['forcing']['k_b']
+        Pr.k_s = namelist['forcing']['k_s']
+        Pr.k_f = namelist['forcing']['k_f']
+        Pr.DT_y = namelist['forcing']['equator_to_pole_dT']
+        Pr.Dtheta_z = namelist['forcing']['lapse_rate']
+        Pr.T_equator = namelist['forcing']['equatorial_temperature']
+
+        PV.P.values      = np.multiply(np.ones((Pr.nlats, Pr.nlons, Pr.n_layers+1), dtype=np.double, order='c'),Pr.pressure_levels)
+
+        if Pr.restart:
+            RS.initialize(Pr, Gr, PV, TS, namelist)
+        else:
+            # PV.T_init            = Pr.T_init
+            PV.Vorticity.values  = np.zeros((Pr.nlats, Pr.nlons, Pr.n_layers),  dtype=np.double, order='c')
+            PV.Divergence.values = np.zeros((Pr.nlats, Pr.nlons, Pr.n_layers),  dtype=np.double, order='c')
+            PV.QT.values         = np.zeros((Pr.nlats, Pr.nlons, Pr.n_layers),   dtype=np.double, order='c')
+            PV.T.values          = np.multiply(np.ones((Pr.nlats, Pr.nlons, Pr.n_layers),   dtype=np.double, order='c'),Pr.T_init)
+
+        PV.physical_to_spectral(Pr, Gr)
+        if namelist['initialize']['noise']:
+             # calculate noise
+             F0=np.zeros(Gr.SphericalGrid.nlm,dtype = np.complex, order='c')
+             fr = spf.sphForcing(Pr.nlons,Pr.nlats,Pr.truncation_number,Pr.rsphere,lmin= 1, lmax= 100, magnitude = 0.05, correlation = 0., noise_type=Pr.noise_type)
+             noise = Gr.SphericalGrid.spectogrd(fr.forcingFn(F0))*Pr.noise_amp
+             # save noise here
+             # np.save('./norm_rand_grid_noise_'+Pr.noise_type+'_.npy',noise)
+             # load noise here
+             # noise = np.load('./norm_rand_grid_noise_'+Pr.noise_type+'_.npy')
+             # add noise
+             PV.T.spectral.base[:,Pr.n_layers-1] = np.add(PV.T.spectral.base[:,Pr.n_layers-1],
+                                                        Gr.SphericalGrid.grdtospec(noise.base))
+        print('layer 3 Temperature min',Gr.SphericalGrid.spectogrd(PV.T.spectral.base[:,Pr.n_layers-1]).min())
+        return
+
+    cpdef initialize_surface(self, Parameters Pr, Grid Gr, PrognosticVariables PV, namelist):
+        self.Sur.initialize(Pr, Gr, PV, namelist)
+        return
+
+    cpdef initialize_turbulence(self, Parameters Pr, namelist):
+        self.Tr.initialize(Pr, namelist)
+        return
+
+    cpdef initialize_convection(self, Parameters Pr, Grid Gr, PrognosticVariables PV, namelist):
+        self.Co.initialize(Pr, Gr, namelist)
+        return
+
+    cpdef initialize_forcing(self, Parameters Pr, Grid Gr, namelist):
+        self.Fo.initialize(Pr, Gr, namelist)
+        return
+
+    cpdef initialize_microphysics(self, Parameters Pr, PrognosticVariables PV, DiagnosticVariables DV,namelist):
+        self.MP.initialize(Pr, PV, DV, namelist)
+        return
+
+    cpdef initialize_io(self, NetCDFIO_Stats Stats):
+        CaseBase.initialize_io(self, Stats)
+        self.Fo.initialize_io(Stats)
+        self.Sur.initialize_io(Stats)
+        return
+
+    cpdef io(self, Parameters Pr, TimeStepping TS, NetCDFIO_Stats Stats):
+        CaseBase.io(self, Pr, TS, Stats)
+        self.Fo.io(Pr, TS, Stats)
+        self.Sur.io(Pr, TS, Stats)
+        # self.Co.io(Pr, TS, Stats)
+        return
+
+    cpdef stats_io(self, PrognosticVariables PV, NetCDFIO_Stats Stats):
+        CaseBase.stats_io(self, PV, Stats)
+        self.Fo.stats_io(Stats)
+        self.Sur.stats_io(Stats)
+        # self.Co.stats_io(Stats)
+        return
+
+    cpdef update(self, Parameters Pr, Grid Gr, PrognosticVariables PV, DiagnosticVariables DV, TimeStepping TS):
+        self.Sur.update(Pr, Gr, PV, DV)
+        self.Fo.update(Pr, Gr, PV, DV)
+        self.Co.update(Pr, Gr, PV, DV)
+        self.MP.update(Pr, PV, DV, TS)
+        self.Tr.update(Pr, Gr, PV, DV)
         return
 
 cdef class HeldSuarez(CaseBase):
